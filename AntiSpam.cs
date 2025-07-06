88c95d468a078d844d3a6bb17bc68da835a0bbb4
mihaigsm2003 <51678688+mihaigsm2003@users.noreply.github.com>
2025-07-06 22:09:34 +0300

    Update AntiSpam.cs

--- AntiSpam.cs
+++ AntiSpam.cs
@@ -2,7 +2,6 @@ using System.Text.Json.Serialization;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
-using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Events;
 
 namespace AntiSpamPlugin;
